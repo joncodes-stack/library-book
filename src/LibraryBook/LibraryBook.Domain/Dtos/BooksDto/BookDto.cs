958463652a46ca6275048ba331c857c787432aa1
jonatas.paz <jonatas.paz@imusica.com.br>
2023-08-15 01:16:28 -0300

    Migração do .net core 6 para o 7

---
+++ src/LibraryBook/LibraryBook.Domain/Dtos/BooksDto/BookDto.cs
@@ -0,0 +1,34 @@
+﻿using LibraryBook.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryBook.Domain.Dtos.BooksDto
+{
+    public class BookDto
+    {
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        public string Title { get; set; }
+
+        public long? IsbnNumber { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        public string Author { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        public string Editor { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        public string Synopsis { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        public Guid IdGender { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        public Guid IdUser { get; set; }
+    }
+}
